766fc7fd30ff98b7486ed80773f79c254238a930
Joshua Lam <joh641@gmail.com>
2019-01-24 12:35:42 -0800

    Add lib folder to eager loaded path
    All classes are eager loaded in production.

--- config/application.rb
+++ config/application.rb
@@ -27,7 +27,8 @@ module AlchemyApi
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
 
-    config.autoload_paths << Rails.root.join('lib')
+    config.autoload_paths   << Rails.root.join('lib')
+    config.eager_load_paths << Rails.root.join('lib')
 
     # Only loads a smaller set of middleware suitable for API only apps.
     # Middleware like session, flash, cookies can be added back manually.
