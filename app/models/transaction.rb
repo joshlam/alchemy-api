22da8ad13468f6c93471e878574e6601ba893b5c
Joshua Lam <joh641@gmail.com>
2019-02-25 00:41:53 -0800

    Add method to print out a summary of transactions
    Adds a convenience method for printing out the information of
    transactions made since a given date, formatted by a given time zone
    (defaults to transactions made during the past day, formatted in PST).

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -5,11 +5,26 @@ class Transaction < ApplicationRecord
     foreign_key: 'transmutation_name',
     primary_key: 'name'
 
+  scope :since, ->(date) { where('created_at >= ?', date) }
+
   def self.record(alchemist_id:, transmutation_name:, mana_earned:)
     self.create! do |transaction|
       transaction.alchemist_id       = alchemist_id
       transaction.transmutation_name = transmutation_name
       transaction.mana_earned        = mana_earned
+    end
+  end
+
+  def self.summary(start_date = 1.day.ago,
+                   time_zone = 'Pacific Time (US & Canada)')
+    since(start_date).map do |transaction|
+      datetime = transaction.created_at.in_time_zone(time_zone)
+
+      [
+        transaction.alchemist.username,
+        transaction.transmutation_name,
+        "#{datetime.month}/#{datetime.day} #{datetime.hour}:#{datetime.min}"
+      ]
     end
   end
 
